34e8d588753519c34fddc710c17ee4460584f29c
mrin2810 <mrinmayeerane2810@gmail.com>
2022-03-29 13:39:57 +0530

    added react-router-dom and Router in the code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 export default function App() {
-  return <div className="app">Signup form goes here</div>;
+  return <Router>
+    <div className='app'>Signup form goes hera</div>
+  </Router>
 }
