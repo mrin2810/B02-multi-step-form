617d0a6cab7d57eb8dbfce0c111a15299d96e9f5
mrin2810 <mrinmayeerane2810@gmail.com>
2022-03-29 16:03:01 +0530

    Added react-hook-form and Setup ProfileForm, Route

--- src/components/SignupForm/01ProfileForm.jsx
+++ src/components/SignupForm/01ProfileForm.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
-
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 export default function ProfileForm() {
-    return <form>
-        I am a Profile Form
-        <input type="submit" value="Next" />
-    </form>
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const navigate = useNavigate();
+
+    function onSubmit(data) {
+        // do whatever we need
+        console.log(data);
+        navigate("/social");
+    }
+    return (
+        <form onSubmit={handleSubmit(onSubmit)} noValidate>
+            <h2>Tell us about yourself</h2>
+            <input 
+                type="text" 
+                name="name" 
+                placeholder="What's your name?" 
+                {...register("name", {
+                    required: true
+                })} 
+            />
+            <p>{errors.name && 'Name is Required'}</p>
+            <input 
+                type="email" 
+                name="email" 
+                placeholder="What's your email?" 
+                {...register("email", {
+                    required: true,
+                    pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                })} 
+            />
+            <p>{errors.email && 'A valid Email is Required'}</p>
+
+            <input type="submit" value="Next" />
+        </form>
+    )
 }
