435134d30a5c8f4aa0cd166c49cc55c136101625
mrin2810 <mrinmayeerane2810@gmail.com>
2022-03-30 17:11:56 +0530

    added style to links and restricted for validation

--- src/components/StepLinks/index.jsx
+++ src/components/StepLinks/index.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useSignupForm } from '../SignupForm/SignupFormContext';
+
+function isEmpty(obj) {
+    return Object.keys(obj).length === 0;
+}
 
 export default function StepLinks() {
-     return (
+    const { profile, social } = useSignupForm();
+
+    // if profile is empty
+    const isProfileDone = !isEmpty(profile);
+    // if social is empty
+    const isSocialDone = !isEmpty(social);
+
+    return (
          <div className='step-links'>
-             <NavLink to='/'>Profile</NavLink>
-             <NavLink to='/social'>Social</NavLink>
-             <NavLink to='/review'>Review</NavLink>
+             <NavLink to='/'>
+                 {isProfileDone ? '❤' : '🤍'}Profile <span /></NavLink>
+             {isProfileDone ? <NavLink to='/social'>
+                 {isSocialDone ? '❤' : '🤍'}Social <span /></NavLink> : <a>{isSocialDone ? '❤' : '🤍'}Social <span /></a>}
+             {isSocialDone && isProfileDone ? <NavLink to='/review' style={{ float: 'right' }}>Review <span /></NavLink> : <a  style={{ float: 'right' }}>Review <span /></a>}
          </div>
      )
 }
